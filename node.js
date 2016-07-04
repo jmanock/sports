1d8120f12a963de5bf9ef5342d1dda257c438378
jmanock <jonmanock@gmail.com>
2016-07-04 06:23:48 -0400

    working with stattleshiip

---
+++ node.js
@@ -0,0 +1,22 @@
+var StattleshipAPI = require('node-stattleship');
+var something = new StattleshipAPI('cfccc6c4a2b20028fb66d881d406f4ca');
+// var params = {
+//   season_id:"mlb-2016",
+//   interval_type:"regularseason",
+//   on:"today"
+// };
+// statleship.players('baseball', 'mlb', params).then(function(players){
+//   players.forEach(function(element, index, array){
+//     console.log(element, index, array);
+//   });
+// });
+// console.log(something.games);
+
+var params = {
+  team_id:"mlb-bos"
+};
+something.games('baseball', 'mlb', params).then(function(games){
+  games.forEach(function(e,i,a){
+    console.log(e.scoreline, e.on);
+  });
+});
